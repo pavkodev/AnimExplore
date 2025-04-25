9a82f8bcf78b8fe2ddb5f60e7d35c1b8dcb0cd73
Tesolite <pavel.kotka@gmail.com>
2025-04-25 14:39:54 +0100

    Added heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import AnimeGroup from "./components/AnimeGroup";
 import HeaderComponent from "./components/HeaderComponent";
+import HeadingComponent from "./components/HeadingComponent";
 
 function App() {
   return (
     <>
       <HeaderComponent />
+      <HeadingComponent heading={"Top Anime"} />
       <AnimeGroup url="https://api.jikan.moe/v4/top/anime" />
     </>
   );
---
+++ src/components/HeadingComponent.tsx
@@ -0,0 +1,5 @@
+const HeadingComponent = (props: { heading: string }) => {
+  return <h1 className="text-white m-4 font-extrabold text-2xl">{props.heading}</h1>;
+};
+
+export default HeadingComponent;
