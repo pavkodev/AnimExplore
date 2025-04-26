a99aeba4f4253a65fe2684c2890a03a75a11f1cd
Tesolite <pavel.kotka@gmail.com>
2025-04-26 20:42:42 +0100

    Hero component now takes in props

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,16 @@ function App() {
       <FilmTvToggle />
       <HeadingComponent heading={"Genre Information"} />
       <GenreInfo />
-      <HeroAnimeCard />
+      <HeroAnimeCard
+        image="https://cdn.myanimelist.net/images/anime/1527/146836l.webp"
+        title={"Enen no Shouboutai: San no Shou"}
+        titleEnglish={"Fire Force Season 3"}
+        studios={["David Production"]}
+        type={"TV"}
+        genres={["Action", "Fantasy", "Sci-Fi"]}
+        trailer={"https://www.youtube.com/watch?v=nz-VCl7yUAw"}
+        synopsis={"Third season of Enen no Shouboutai."}
+      />
       <HeadingComponent heading={"New Season Releases"} />
       <AnimeGroup url="https://api.jikan.moe/v4/seasons/now" />
       <HeadingComponent heading={"Top Anime"} />
