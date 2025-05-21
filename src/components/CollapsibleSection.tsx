e4fa8608dbcab843d3987a753c098eb6e1e6b1af
Tesolite <pavel.kotka@gmail.com>
2025-05-21 04:39:34 +0100

    Minor spacing improvement to collapsible section headings

--- src/components/CollapsibleSection.tsx
+++ src/components/CollapsibleSection.tsx
@@ -7,7 +7,7 @@ const CollapsibleSection = (props: {
 }) => {
   return (
     <details className="mt-12" open={props.openByDefault ? true : false}>
-      <summary className="cursor-pointer p-2 text-orange-400 hover:bg-slate-700">
+      <summary className="cursor-pointer p-2 pl-4 text-orange-400 hover:bg-slate-700">
         <strong className="p-2 text-2xl font-extrabold text-white">
           {props.heading}
         </strong>
