b656d30a674338e77a113eaacb71dcfa3c9e1a16
Tesolite <pavel.kotka@gmail.com>
2025-05-21 20:46:41 +0100

    watchlist button added to hero cards

--- src/components/HeroAnimeCard.tsx
+++ src/components/HeroAnimeCard.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import { HeroInfo } from "../types/types";
+import { WatchlistContext } from "../contexts/WatchlistContext";
 
 const HeroAnimeCard = (props: HeroInfo) => {
+  const { watchlist, setWatchlist } = useContext(WatchlistContext);
+  const inWatchlist = watchlist.includes(props.id);
   return (
     <>
       {props.loading ? (
@@ -19,12 +23,29 @@ const HeroAnimeCard = (props: HeroInfo) => {
           </div>
         </div>
       ) : (
-        <div className="embla__slide m-2 flex w-[80vw] snap-center flex-col items-center justify-center rounded border-2 border-orange-600 bg-gray-950 pb-4 text-white lg:w-max lg:flex-row lg:pr-4 lg:pb-0">
+        <div className="embla__slide relative m-2 flex w-[80vw] snap-center flex-col items-center justify-center rounded border-2 border-orange-600 bg-gray-950 pb-4 text-white lg:w-max lg:flex-row lg:pr-4 lg:pb-0">
           <img
             src={props.image}
             alt="placeholder"
             className="self-stretch rounded mask-b-from-0 lg:mask-r-from-0 lg:mask-b-from-100%"
           />
+          <button
+            onClick={() => {
+              if (!inWatchlist) {
+                setWatchlist(props.id);
+              }
+            }}
+            className={`absolute top-0 left-0 m-2 flex cursor-pointer items-center justify-center rounded bg-slate-800/70 p-2 transition-all hover:bg-slate-800 active:translate-y-0.5 ${inWatchlist ? "text-cyan-400" : ""}`}
+          >
+            <svg
+              className="h-6 w-6 fill-current pr-1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 -960 960 960"
+            >
+              <path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z" />
+            </svg>
+            {inWatchlist ? "In watchlist" : "Add to watchlist"}
+          </button>
           <div className="flex flex-1 flex-col items-center justify-center text-center lg:my-4 lg:ml-8 lg:w-[50ch]">
             <h2
               className="mb-2 line-clamp-3 text-xl font-bold"
